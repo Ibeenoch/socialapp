3b8b494782e8c22b692a0eb80b83a39d227239e0
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2022-12-12 22:44:02 +0100

    Update Comments.js

--- src/component/SideOne/Comments.js
+++ src/component/SideOne/Comments.js
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
 
-const Comments = ({post, times}) => {
-    const { user } = useSelector((state) => state.user)
+const Comments = ({post, times, }) => {
+    const  us = useSelector((state) => post ? state.user.allUser.find((as) => as._id === post.user ) : null)
+
     const ji = new Date().getTime();
 
-
-console.log('sxc')
     return (
              <div style={{display: 'flex'}}>
-             <img src={user.user.profilepics.url} style={{width: '30px', height: '30px', borderRadius: '50%',  padding: '0.7rem'}} />
+             <img src={us.profilepics.url} style={{width: '30px', height: '30px', borderRadius: '50%',  padding: '0.7rem'}} />
              <div style={{width: '50%', height: 'max-content', background:'gray', color: 'white', fontSize: '1rem', borderRadius: '1rem', padding: '0.7rem', margin: '0.7rem'}}>
                  <p style={{fontSize: '14px'}} >{post.comment} </p>
-           <p style={{fontSize: '10px'}}><small>{moment(ji).toNow()}</small></p>
+           <p style={{fontSize: '10px'}}><small>{moment(ji).format("HH:mm")}</small></p>
             </div>
           </div>
     )
