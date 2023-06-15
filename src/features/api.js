26197a7679fd9b922e008dbd1593bdf55b2e3acf
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2023-06-15 21:53:17 +0100

    Update api.js

--- src/features/api.js
+++ src/features/api.js
@@ -27,7 +27,7 @@ return data
   export const getPerson = async(id) => {
 
       const { token} = JSON.parse(localStorage.getItem('user'))
- console.log("getPersonApi: ", token);
+ console.log("getPersonApi: ", id, token);
       const option = {
        method: 'GET',
        headers: {
