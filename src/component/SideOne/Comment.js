4574d474f9b2df5b5bbedc3f9a23107eca5c27e9
Ibeenoch <94024099+Ibeenoch@users.noreply.github.com>
2024-01-03 17:14:59 +0100

    Update Comment.js

--- src/component/SideOne/Comment.js
+++ src/component/SideOne/Comment.js
@@ -60,20 +60,21 @@ const Comment = () => {
 
  
   return (
-    <div style={{ width: '100℅', height: '80vh', margin: '1rem 0.5rem', borderRadius: '1.2rem'}}>
-        <Box sx={{ width: '100%', height:'80vh', background: 'white', borderRadius: '1.2rem', boxShadow: '0 0 1rem gray'}} >
+    <div style={{ width: '100℅', height: '82vh', margin: '1rem 0.5rem 1rem 0rem', borderRadius: '1.2rem'}}>
+        <Box sx={{ width: '100%', height:'82vh', background: 'white', borderRadius: '1.2rem', boxShadow: '0 0 1rem gray'}} >
           <div style={{background: 'gray', borderTopLeftRadius: '1.2rem', borderTopRightRadius: '1.2rem', color: 'white'}}>
               <Typography variant='h5' align='center' style={{ fontSize: '0.8rem', paddingTop: '0.7rem'}} >Comment</Typography>
           </div>
           
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', background: 'gray', padding: '1rem'}}>
          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', columnGap: '1.2rem'}}>
-              <div onClick={backHome} style={{cursor: 'pointer'}}>
-                  <ArrowBack />
-              </div>
-              <div>
-                <img src={user.user.profilepics.url || profile.fetchProfile.map((item) => item.profilepics.url)} style={{width: '40px', height: '40px', borderRadius: '50%'}} />
-              </div>  
+              <div onClick={backHome} style={{cursor: 'pointer', display: 'flex'}}>
+                <ArrowBack />
+                <div style={{ margin: '0rem 0.2rem'}}>
+                 <img src={user.user.profilepics.url || profile.fetchProfile.map((item) => item.profilepics.url)} style={{width: '40px', height: '40px', borderRadius: '50%'}} />
+                </div>
+             </div>
+              
               <div>
                 <Typography style={{ fontSize: '1rem'}} variant='h4'>{user.user.name} </Typography>
              </div>
@@ -95,8 +96,8 @@ const Comment = () => {
 
          
         <form onSubmit={handlecomment}>
-             <div style={{ display: 'flex', marginTop:'60vh', justifyContent:'center', alignItems:'center', alignContent:'center', borderRadius: '1.2rem', background: 'white', boxShadow: '0 0 0.4rem gray', width: '100%', padding: '0.5rem' }}>
-              <TextField style={{ fontSize: '1rem', border:'none', paddingLeft:'1.5rem', textAlign:'center', marginTop:'-1rem'}} type='text'  fullWidth label="write a comment " name='comments[omment]' value={word.comments.comment} onChange={handleChange} />
+             <div style={{ display: 'flex', marginTop:'62vh', justifyContent:'center', alignItems:'center', alignContent:'center', borderRadius: '0.3rem', background: 'white', boxShadow: '0 0 0.4rem gray', width: '80%', padding: '0.5rem' }}>
+              <TextField style={{ fontSize: '1rem', border:'none', textAlign:'center', marginTop:'-1rem'}} type='text'  fullWidth label="write a comment " name='comments[omment]' value={word.comments.comment} onChange={handleChange} />
              <button  style={{ fontSize: '1rem', border:'none', background:'none', cursor:'pointer', marginRight:'0.7rem'}} type='submit' >
                  <Send />
              </button>
